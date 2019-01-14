cff220f821362e4806031b9db791d32f661da0e7
Anil Kumar krishanshetty <anilbms75@gmail.com>
2019-01-14 21:35:42 +0100

    Add sample Rebass UI component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+    Box,
+    Card,
+    Image,
+    Heading,
+    Text
+} from 'rebass';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <Box width={256}>
+                        <Card
+                            p={1}
+                            borderRadius={2}
+                            boxShadow='0 0 16px rgba(0, 0, 0, .25)'>
+                            <Image src="https://via.placeholder.com/150"/>
+                            <Box px={2}>
+                                <Heading as='h3'>
+                                    Card
+                                </Heading>
+                                <Text fontSize={0}>
+                                    Small meta text
+                                </Text>
+                            </Box>
+                        </Card>
+                    </Box>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
